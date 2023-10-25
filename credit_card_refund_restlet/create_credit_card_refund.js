25967cfa08cfee7af3632818dec9883f8edd4154
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2023-10-25 08:41:31 +0530

    Adding semicolon

--- credit_card_refund_restlet/create_credit_card_refund.js
+++ credit_card_refund_restlet/create_credit_card_refund.js
@@ -42,7 +42,7 @@ function (record) {
         creditCardRefund.setValue({ fieldId: 'subsidiary', value: requestBody.subsidiary.internalId });
         creditCardRefund.setValue({ fieldId: 'account', value: requestBody.account.internalId });
         creditCardRefund.setValue({ fieldId: 'trandate', value: new Date(requestBody.tranDate) });
-        creditCardRefund.setValue({ fieldId: 'tranid', value: requestBody.tranid })
+        creditCardRefund.setValue({ fieldId: 'tranid', value: requestBody.tranid });
         creditCardRefund.setValue({ fieldId: 'location', value: requestBody.location.internalId });
         creditCardRefund.setValue({ fieldId: 'department', value: requestBody.department.internalId });
         creditCardRefund.setValue({ fieldId: 'currency', value: requestBody.currency.internalId });

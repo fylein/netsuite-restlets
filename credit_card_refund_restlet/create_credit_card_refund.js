417f862fb1437dd7b0e51a41f5f75eecc9cc5cc4
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2024-09-25 11:14:48 +0530

    Add get update methods (#7)
    * Add get update methods
    
    * Add all the fields to the top level edit
    
    * Modify Get method
    
    * safely handle values in put
    
    * fix put
    
    ---------
    
    Co-authored-by: GitHub Actions <integrations@fylehq.com>

--- credit_card_refund_restlet/create_credit_card_refund.js
+++ credit_card_refund_restlet/create_credit_card_refund.js
@@ -3,30 +3,145 @@
  * @NScriptType Restlet
  * @NModuleScope Public
  */
-define(['N/record'],
-/**
- * @param {record} record
- */
-function (record) {
-    /**
-     * Function called upon sending a POST request to the RESTlet.
-     *
-     * @param {string | Object} requestBody - The HTTP request body; request body will be passed into function as a
-     * string when request Content-Type is 'text/plain'
-     * or parsed into an Object when request Content-Type is 'application/json'
-     * (in which case the body must be a valid JSON)
-     * @returns {string | Object} HTTP response body; return string when request Content-Type is 'text/plain';
-     * return Object when request Content-Type is 'application/json'
-     * @since 2015.2
-     */
+define(['N/record'], function (record) {
+    
+    // Function called upon sending a POST request to create a new credit card refund record
     function doPost(requestBody) {
         var creditCardRefund = CreateNetSuiteCreditCardRefund(requestBody);
         if (creditCardRefund.success) {
-            log.debug('CCC', creditCardRefund);
+            log.debug('CCC Refund', creditCardRefund);
             return creditCardRefund;
         } else {
-            log.debug('CCC refund', 'Fail');
+            log.debug('CCC Refund', 'Fail');
             return creditCardRefund;
+        }
+    }
+
+    // Function called upon sending a GET request to retrieve a record
+    function doGet(requestParams) {
+        try {
+            // Load the credit card refund record in dynamic mode
+            var creditCardRefund = record.load({
+                type: record.Type.CREDIT_CARD_REFUND,
+                id: requestParams.internalId,
+                isDynamic: true
+            });
+
+            return creditCardRefund;
+        } catch (e) {
+            log.error('Error in doGet', e);
+            return { success: false, message: e.message };
+        }
+    }
+
+    // Function called upon sending a PUT request to update a record
+    function doPut(requestBody) {
+        try {
+            var creditCardRefund = record.load({
+                type: record.Type.CREDIT_CARD_REFUND,
+                id: requestBody.internalId,
+                isDynamic: true
+            });
+
+            if (requestBody.entity) { creditCardRefund.setValue({ fieldId: 'entity', value: requestBody.entity.internalId }); }
+            if (requestBody.account) { creditCardRefund.setValue({ fieldId: 'account', value: requestBody.account.internalId }); }
+            if (requestBody.tranDate) {creditCardRefund.setValue({ fieldId: 'trandate', value: new Date(requestBody.tranDate) }); }
+            if (requestBody.tranid) { creditCardRefund.setValue({ fieldId: 'tranid', value: requestBody.tranid }); }
+            if (requestBody.location) { creditCardRefund.setValue({ fieldId: 'location', value: requestBody.location.internalId }); }
+            if (requestBody.department) { creditCardRefund.setValue({ fieldId: 'department', value: requestBody.department.internalId }); }
+            if (requestBody.class) { creditCardRefund.setValue({ fieldId: 'class', value: requestBody.class.internalId }); }
+            if (requestBody.currency) { creditCardRefund.setValue({ fieldId: 'currency', value: requestBody.currency.internalId }); }
+            if (requestBody.memo) { creditCardRefund.setValue({ fieldId: 'memo', value: requestBody.memo }); }
+            if (requestBody.externalId) { creditCardRefund.setValue({ fieldId: 'externalid', value: requestBody.externalId }); }
+
+            // First, remove existing expense lines
+            var expenseLineCount = creditCardRefund.getLineCount({ sublistId: 'expense' });
+            for (var i = expenseLineCount - 1; i >= 0; i--) {
+                creditCardRefund.removeLine({ sublistId: 'expense', line: i });
+            }
+
+            // Add the new expense lines
+            var expenses = requestBody.expenses;
+            expenses.forEach(function (expense) {
+                creditCardRefund.selectNewLine({ sublistId: 'expense' });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'account',
+                    value: expense.account.internalId
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'amount',
+                    value: expense.amount
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'memo',
+                    value: expense.memo
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'department',
+                    value: expense.department.internalId
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'class',
+                    value: expense.class.internalId
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'location',
+                    value: expense.location.internalId
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'customer',
+                    value: expense.customer.internalId
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'isbillable',
+                    value: expense.isBillable
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'taxcode',
+                    value: expense.taxCode.internalId
+                });
+
+                creditCardRefund.setCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'taxamount',
+                    value: expense.taxAmount
+                });
+
+                expense.customFieldList.forEach(function (customField) {
+                    creditCardRefund.setCurrentSublistValue({
+                        sublistId: 'expense',
+                        fieldId: customField.scriptId,
+                        value: customField.value
+                    });
+                });
+
+                creditCardRefund.commitLine({ sublistId: 'expense' });
+            });
+
+            var id = creditCardRefund.save();
+            return { internalId: id, success: true };
+
+        } catch (e) {
+            log.error('Error in doPut', e);
+            return { success: false, message: e.message };
         }
     }
 
@@ -46,18 +161,13 @@ function (record) {
         creditCardRefund.setValue({ fieldId: 'location', value: requestBody.location.internalId });
         creditCardRefund.setValue({ fieldId: 'department', value: requestBody.department.internalId });
         creditCardRefund.setValue({ fieldId: 'class', value: requestBody.class.internalId });
-        creditCardRefund.setValue({ fieldId: 'department', value: requestBody.department.internalId });
         creditCardRefund.setValue({ fieldId: 'currency', value: requestBody.currency.internalId });
         creditCardRefund.setValue({ fieldId: 'memo', value: requestBody.memo });
         creditCardRefund.setValue({ fieldId: 'externalid', value: requestBody.externalId });
 
+        creditCardRefund.selectNewLine({ sublistId: 'expense' });
 
-        creditCardRefund.selectNewLine({
-            sublistId: 'expense'
-        });
-
-
-        var expenses = requestBody.expenses
+        var expenses = requestBody.expenses;
 
         expenses.forEach(function (expense) {
             creditCardRefund.setCurrentSublistValue({
@@ -128,10 +238,7 @@ function (record) {
                 });
             });
 
-            creditCardRefund.commitLine({
-                sublistId: 'expense'
-            });
-
+            creditCardRefund.commitLine({ sublistId: 'expense' });
         });
         log.debug('ccc_refund', creditCardRefund);
         var id = creditCardRefund.save({});
@@ -140,8 +247,10 @@ function (record) {
         return { internalId: id, success: true };
     }
 
+    // Return the RESTlet methods for each HTTP method
     return {
+        'get': doGet,
         'post': doPost,
+        'put': doPut
     };
-
 });
